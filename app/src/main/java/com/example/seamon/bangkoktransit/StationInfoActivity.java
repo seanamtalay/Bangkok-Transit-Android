0f4f9bc20d20187f3d2e3d2bcfb206e9b76dea67
seanamtalay <sea_skin@hotmail.com>
2018-07-18 13:37:55 -0700

    Finished adding string resource + linked corresponding data to the station info page

--- app/src/main/java/com/example/seamon/bangkoktransit/StationInfoActivity.java
+++ app/src/main/java/com/example/seamon/bangkoktransit/StationInfoActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class StationInfoActivity extends AppCompatActivity {
     private static final String TAG = "StationInfoActivity";
@@ -17,9 +18,34 @@ public class StationInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_station_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        TextView stationInfoName = findViewById(R.id.station_info_name);
+        TextView stationInfoLine = findViewById(R.id.station_info_line);
+        TextView stationInfoPlatform = findViewById(R.id.station_info_platform);
+        TextView stationInfoExit = findViewById(R.id.station_info_exit);
 
         current_station = getIncomingIntent();
+
+        //Get the station code
+        String[] splitedName = current_station.split("\\s+");
+        String stationCode = splitedName[0];
+
+        //get the station info based on the stationCode
+        int resId = this.getResources().getIdentifier(stationCode,"array",this.getPackageName()); // might cause a bug (return 0)
+        String[] stringArray = getResources().getStringArray(resId);
+        String receivedName = stringArray[0];
+        String receivedLine = stringArray[1];
+        String receivedPlatform = stringArray[2];
+        String receivedExit = stringArray[3];
+
+        //change them accordingly
+        stationInfoName.setText(receivedName);
+        stationInfoLine.setText(receivedLine);
+        stationInfoPlatform.setText(receivedPlatform);
+        stationInfoExit.setText(receivedExit);
+
         toolbar.setTitle(current_station);
+
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
